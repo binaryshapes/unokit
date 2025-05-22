65d1a90f7b272ccb5909e0a71acae7d29cb254c8
Carlos Cosming <carlos@kingly.cl>
2025-05-22 14:22:00 -0400

    ✨ feat(vitest): refactor Vitest configuration into builder module
    - Moved the base configuration from `base.ts` to a new `builder.ts` file, allowing for a more modular approach to defining Vitest configurations.
    - Updated `base.ts` to export the new `defineVitestConfig` function, which encapsulates the configuration logic.
    - Enhanced the configuration options to include additional parameters for better customization.

---
+++ packages/vitest/src/builder.ts
@@ -0,0 +1,152 @@
+/*
+ * This file is part of the UnoKit project.
+ *
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { type TestProjectConfiguration, defineConfig } from 'vitest/config';
+
+/**
+ * Options for Vitest configuration.
+ */
+type VitestOptions = {
+  /**
+   * Whether to use globals.
+   * This need to set "types": ["vitest/globals"] in tsconfig.json.
+   *
+   * @defaultValue true
+   */
+  globals?: boolean;
+  /**
+   * Whether to watch for changes.
+   *
+   * @defaultValue false
+   */
+  watch?: boolean;
+  /**
+   * Whether to use UI.
+   *
+   * @defaultValue false
+   */
+  ui?: boolean;
+  /**
+   * The directory to run the tests.
+   *
+   * @defaultValue './tests'
+   */
+  dir?: string;
+  /**
+   * Whether to disable console intercept.
+   *
+   * @defaultValue true
+   */
+  disableConsoleIntercept?: boolean;
+  /**
+   * The thresholds for the tests.
+   *
+   * @defaultValue 90
+   */
+  thresholds?: number;
+  /**
+   * The projects to run the tests.
+   *
+   * @defaultValue undefined
+   */
+  projects?: { mode: 'add' | 'replace'; config: TestProjectConfiguration[] };
+};
+
+/**
+ * The default configuration for Vitest.
+ * Unokit team prefer to use this configuration.
+ */
+const defaultConfig: VitestOptions = {
+  globals: true,
+  watch: false,
+  ui: false,
+  dir: './tests',
+  thresholds: 90,
+  disableConsoleIntercept: true,
+  projects: undefined,
+};
+
+/**
+ * Base configuration for Vitest.
+ *
+ * @param config - The configuration for Vitest.
+ * @returns The configuration for Vitest.
+ */
+const defineVitestConfig = (config: VitestOptions = defaultConfig) =>
+  defineConfig({
+    test: {
+      // This need to set "types": ["vitest/globals"] in tsconfig.json.
+      globals: config.globals,
+      watch: config.watch,
+      ui: config.ui,
+      dir: config.dir,
+      environment: 'node',
+      reporters: [
+        [
+          'verbose',
+          {
+            isTTY: true,
+            summary: false,
+          },
+        ],
+      ],
+      disableConsoleIntercept: config.disableConsoleIntercept,
+      outputFile: {
+        ifNoTests: 'warn',
+        ifFailed: 'error',
+      },
+      coverage: {
+        reportsDirectory: './coverage',
+        ignoreEmptyLines: true,
+        enabled: false,
+        provider: 'v8',
+        include: [
+          'src/**/*.ts',
+        ],
+        exclude: [
+          '**/index.ts',
+        ],
+        reporter: [
+          ['text', { skipEmpty: true }],
+          ['html', { skipEmpty: true }],
+        ],
+        cleanOnRerun: true,
+        thresholds: {
+          statements: config.thresholds,
+          branches: config.thresholds,
+          functions: config.thresholds,
+          lines: config.thresholds,
+        },
+      },
+      workspace:
+        config.projects?.mode === 'add'
+          ? [
+              {
+                test: {
+                  name: 'runtime',
+                  include: ['tests/**/*.spec.ts'],
+                },
+              },
+              {
+                test: {
+                  name: 'types',
+                  include: ['tests/**/*.spec-d.ts'],
+                  typecheck: {
+                    enabled: true,
+                    only: true,
+                    tsconfig: './tsconfig.spec.json',
+                  },
+                },
+              },
+              ...(config.projects?.config ?? []),
+            ]
+          : config.projects?.config,
+    },
+  });
+
+export default defineVitestConfig;
--- packages/vitest/src/index.ts
+++ packages/vitest/src/index.ts
@@ -1,1 +1,2 @@
 export { default as vitestBase } from './base';
+export { default as defineVitestConfig } from './builder';
